a70a48199c2e760373569a5db721cb2da100278e
Deepak Kumar Verma <128968635+DeepakKumarVermaRamgarh@users.noreply.github.com>
2023-07-01 12:01:11 +0530

    Adding functionality to show coupon and copy the copon code in clipboard

--- script.js
+++ script.js
@@ -4,7 +4,12 @@
 const wheel = document.querySelector('.wheel');
 const spinBtn = document.querySelector('.spin-btn');
 const prizeSpan = document.querySelectorAll('.amount span');
+const couponCode = document.getElementById('coupon-code');
+const couponBtn = document.getElementById('coupon-btn');
 const wheelContainer = document.querySelector('.container');
+const couponContainer = document.querySelector('.coupon-container');
+const spinningAudio = document.getElementById('wheel-spinning');
+const cheerAudio = document.getElementById('cheering');
 
 // Array to store the prize values
 let prizes = [];
@@ -17,9 +22,8 @@ let counter = 0;
 
 // Storing the prize values in reverse order
 prizeSpan.forEach((prize) => {
-    prizes.unshift(prize.innerText);
+  prizes.unshift(prize.innerText);
 });
-
 
 // Initial rotation of the wheel
 wheel.style.transform = `rotate(+24.5deg)`;
@@ -27,49 +31,69 @@ wheel.style.transform = `rotate(+24.5deg)`;
 // Event listener for spin button click
 let val = 0;
 spinBtn.addEventListener('click', () => {
-    // Setting transition and playing spinning audio
-    wheel.style.transition = "transform 5s ease-in-out";
+  // Setting transition and playing spinning audio
+  wheel.style.transition = "transform 5s ease-in-out";
+  spinBtn.classList.remove('inOut');
 
-    counter++;
+  counter++;
+  spinningAudio.play();
 
-    // Calculating the rotation value for each spin
-    if (counter > 1) {
-        val += Math.ceil(Math.random() * 3600);
-    } else {
-        val += 170;
+  // Calculating the rotation value for each spin
+  if (counter > 1) {
+    val += Math.ceil(Math.random() * 3600);
+  } else {
+    val += 170;
+  }
+
+  // Rotating the wheel gradually
+  const spinInterval = setInterval(() => {
+    wheel.style.transform = `rotate(${val}deg)`;
+  }, 500);
+
+  // Stopping the wheel after a certain time
+  setTimeout(() => {
+    clearInterval(spinInterval);
+    spinningAudio.pause();
+    spinBtn.classList.add('inOut');
+
+    // Calculating the prize won
+    val = Math.floor((val % 360) / 45) % 8;
+
+    if (prizes[val] !== "Try Again") {
+      message = "Congratulations, you won " + prizes[val];
     }
 
-    // Rotating the wheel gradually
-    const spinInterval = setInterval(() => {
-        wheel.style.transform = `rotate(${val}deg)`;
-    }, 500);
-
-    // Stopping the wheel after a certain time
-    setTimeout(() => {
-        clearInterval(spinInterval);
-
-        // Calculating the prize won
-        val = Math.floor((val % 360) / 45) % 8;
-
-        if (prizes[val] !== "Try Again") {
-            message = "Congratulations, you won " + prizes[val];
-        }
-
-        if (message !== "") {
-            // Displaying the coupon code
-            console.log(message);
-        } else {
-            // Alerting user to try again if no prize won
-            alert('Please Try Again...!');
-            wheel.style.transition = "";
-            initialState();
-        }
-    }, 5000);
+    if (message !== "") {
+      // Displaying the coupon code
+      const couponCard = document.querySelector('.coupon-card h3');
+      couponCard.innerText = message;
+      wheelContainer.classList.toggle('hide');
+      couponContainer.classList.toggle('hide');
+      let code = prizes[val].split('%')[0];
+      couponCode.innerHTML = `FLAT${code}`;
+      cheerAudio.play();
+    } else {
+      // Alerting user to try again if no prize won
+      alert('Please Try Again...!');
+      wheel.style.transition = "";
+      initialState();
+    }
+  }, 5000);
 });
 
 // Function to reset the wheel to its initial state
 const initialState = () => {
-    wheel.style.transform = `rotate(-${val}deg)`;
-    wheel.style.transform = `rotate(+24.5deg)`;
-    val = 0;
+  wheel.style.transform = `rotate(-${val}deg)`;
+  wheel.style.transform = `rotate(+24.5deg)`;
+  val = 0;
 };
+
+// Event listener for coupon button click
+couponBtn.addEventListener('click', () => {
+  // Copying the coupon code to clipboard
+  navigator.clipboard.writeText(couponCode.innerHTML);
+  couponBtn.innerHTML = "COPIED";
+  setTimeout(() => {
+    couponBtn.innerHTML = "COPY CODE";
+  }, 3000);
+});
